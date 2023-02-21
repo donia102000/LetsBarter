ca6d6cf656a0b2fa641ad84bc812e61213992881
Ghazoua23 <123769489+Ghazoua23@users.noreply.github.com>
2023-02-21 13:44:46 +0100

    crud (évenement,reservation)

---
+++ piDev/src/pidev/entities/Reservation.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pidev.entities;
+
+
+/**
+ *
+ * @author Ghazoua
+ */
+public class Reservation {
+    private int id_reservation;
+    private int nbrPace;
+    private int id_client;
+    private int id_evenement;
+
+    public Reservation() {
+    }
+
+    public Reservation(int id_reservation, int nbrPace, int id_client, int id_evenement) {
+        this.id_reservation = id_reservation;
+        this.nbrPace = nbrPace;
+        this.id_client=id_client;
+        this.id_evenement=id_evenement;
+    }
+
+    public Reservation(int id_reservation, int nbrPace) {
+        this.id_reservation = id_reservation;
+        this.nbrPace = nbrPace;
+    }
+    
+
+    public Reservation(int nbrPace) {
+        this.nbrPace = nbrPace;
+    }
+    
+
+    public int getId_reservation() {
+        return id_reservation;
+    }
+
+    public int getNbrPace() {
+        return nbrPace;
+    }
+
+    public int getId_client() {
+        return id_client;
+    }
+
+    public int getId_evenement() {
+        return id_evenement;
+    }
+
+    public void setId_reservation(int id_reservation) {
+        this.id_reservation = id_reservation;
+    }
+
+    public void setNbrPace(int nbrPace) {
+        this.nbrPace = nbrPace;
+    }
+
+    public void setId_client(int id_client) {
+        this.id_client = id_client;
+    }
+
+    public void setId_evenement(int id_evenement) {
+        this.id_evenement = id_evenement;
+    }
+
+   
+}
---
+++ piDev/src/pidev/services/ServiceEvenement.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pidev.services;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import pidev.entities.Evenement;
+import pidev.utils.DataBase1;
+
+/**
+ *
+ * @author Ghazoua
+ */
+public class ServiceEvenement implements IService <Evenement>{
+    Connection cnx = DataBase1.getInstance().getCnx();
+
+    @Override
+    public void ajouter(Evenement e) {
+        //Utilisateur user1 = inscrit().getId()
+        try {
+            String req = "INSERT INTO `evenement` (`libelle`,`date`, `lieu`, `nbrPlaceMax`) VALUES ('" + e.getLibelle()+ "', '" +e.getDate()+ "','" + e.getLieu()+ "','" + e.getNbrPlaceMax()+ "')";
+            Statement st = cnx.createStatement();
+            st.executeUpdate(req);
+            System.out.println("Evenement created !");
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    @Override
+    public void supprimer(int id) {
+        try {
+            String req = "DELETE FROM `evenement` WHERE `idEvenement`= " + id;
+            Statement st = cnx.createStatement();
+            st.executeUpdate(req);
+            System.out.println("Evenement deleted !");
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    @Override
+    public void modifier(int id, String libelle,String date,String lieu, int nbrPlace) {
+        try {
+            String req = "UPDATE `evenement` SET `libelle` = '" +libelle+ "',`date` = '" +date+ "', `lieu` = '" +lieu+ "', `nbrPlaceMax` = '" +nbrPlace+ "' WHERE `evenement`.`idEvenement` = " + id;
+            Statement st = cnx.createStatement();
+            st.executeUpdate(req);
+            System.out.println("Evenement updated !");
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    @Override
+    public List<Evenement> afficher() {
+        List<Evenement> list = new ArrayList<>();
+        try {
+            String req = "Select * from evenement";
+            Statement st = cnx.createStatement();
+            ResultSet rs = st.executeQuery(req);
+            while (rs.next()) {
+                Evenement e = new Evenement(rs.getInt(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getInt(5),rs.getInt(6));
+                list.add(e);
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        return list;
+    }
+
+    @Override
+    public Evenement rechercher(int id) {
+        Evenement e = null;
+        try {
+            String req = "Select * from `evenement` WHERE `idEvenement` = " +id;
+            Statement st = cnx.createStatement();
+            ResultSet rs = st.executeQuery(req);
+            while (rs.next()) {
+                e = new Evenement(rs.getInt(1), rs.getString(2), rs.getString(3),rs.getString(4),rs.getInt(5),rs.getInt(6));
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        return e;
+    }
+
+ }
+   
